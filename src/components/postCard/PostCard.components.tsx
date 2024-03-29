7c5e0e95165fa1fc5550ccf6432fd8b635ad2c4f
kwinkich <vankord21@gmail.com>
2024-03-29 17:43:24 +0200

    add props for styles

--- src/components/postCard/PostCard.components.tsx
+++ src/components/postCard/PostCard.components.tsx
@@ -1,10 +1,16 @@
 import { Post } from '../../types/Post';
 
-export const PostCard = ({ data }: { data: Post }) => {
+export const PostCard = ({
+	data,
+	...props
+}: {
+	data: Post;
+	className: string;
+}) => {
 	return (
 		<div>
 			<div>
-				<h1>{data.title}</h1>
+				<h1 className={' text-3xl' + props.className}>{data.title}</h1>
 				{data.tags.map((tag) => {
 					return <span key={tag}>{tag}</span>;
 				})}
