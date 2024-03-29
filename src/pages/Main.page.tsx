1c354bf2074a80b9dc10fc202fbc4210f70bafcc
kwinkich <vankord21@gmail.com>
2024-03-29 19:02:03 +0200

    update code & styles

--- src/pages/Main.page.tsx
+++ src/pages/Main.page.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Button } from '../components/Button/Button';
 import { PostCard } from '../components/postCard/PostCard.components';
 import { Post } from '../types/Post';
 
@@ -19,12 +20,19 @@ export default function MainPage() {
 
 	return (
 		<div className='container text-center'>
-			<div className='text-color text-2xl'>AdminPanel</div>
+			<div className='text-color text-2xl mb-10'>AdminPanel</div>
 			<div className='max-w-max block-center flex flex-col'>
-				{postData.length !== 0 &&
+				<h2 className='text-color text-xl mb-5'>Posts</h2>
+				{postData.length !== 0 ? (
 					postData.map((post) => {
 						return <PostCard key={post._id} data={post} />;
-					})}
+					})
+				) : (
+					<>
+						<h2 className='text-color text-xl mb-10'>No post yet</h2>
+						<Button>Create Post</Button>
+					</>
+				)}
 			</div>
 		</div>
 	);
