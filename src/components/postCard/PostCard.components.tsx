28cb5f57177bf8f5ba4933bef7ce0c5241f6e22c
kwinkich <vankord21@gmail.com>
2024-03-29 17:46:38 +0200

    fix bug

--- src/components/postCard/PostCard.components.tsx
+++ src/components/postCard/PostCard.components.tsx
@@ -1,16 +1,10 @@
 import { Post } from '../../types/Post';
 
-export const PostCard = ({
-	data,
-	...props
-}: {
-	data: Post;
-	className: string;
-}) => {
+export const PostCard = ({ data }: { data: Post }) => {
 	return (
 		<div>
 			<div>
-				<h1 className={' text-3xl' + props.className}>{data.title}</h1>
+				<h1 className=' text-3xl'>{data.title}</h1>
 				{data.tags.map((tag) => {
 					return <span key={tag}>{tag}</span>;
 				})}
